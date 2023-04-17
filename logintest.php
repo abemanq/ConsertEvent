40b3fe76ccd5f85a7d4e0c9280fecfdd09b2a3ee
Luqman <luqman.vanilla011@gmail.com>
2023-04-17 12:44:51 +0800

    adjusttt

---
+++ logintest.php
@@ -0,0 +1,52 @@
+<html>
+    <body>
+        <?php 
+            //connect database
+            $conn = mysqli_connect('localhost', 'root', '', 'Consert');
+
+            //initialize variable
+            $uname = $_POST['username'];
+            $pwd = $_POST['pwd'];
+
+            session_start();
+            if (isset($uname) && isset($pwd)) {
+                function validate($data){
+                   $data = trim($data);
+                   $data = stripslashes($data);
+                   $data = htmlspecialchars($data);
+                   return $data;
+                }
+                $uname = validate($uname);
+                $pwd = validate($pwd);            
+                if (empty($uname)) {
+                    echo("Location: logintest.html");
+                    exit();
+                }else if(empty($pwd)){
+                    header("Location: logintest.html");
+                    exit();
+                }else{
+                    $sql = "SELECT * FROM userprofile WHERE username='$uname' AND password='$pwd'";
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) === 1) {
+                        $row = mysqli_fetch_assoc($result);
+                        if ($row['username'] === $uname && $row['password'] === $pwd) {
+                            echo "Logged in!";
+                            $_SESSION['username'] = $row['username'];
+                            header("Location: ticket.html");
+                            exit();
+                        }else{
+                            header("Location:logintest.html");
+                            exit();
+                        }
+                    }else{
+                        header("Location:logintest.html");
+                        exit();
+                    }
+                }
+            }else{
+                header("Location:logintest.html");
+                exit();
+            }
+        ?>
+    </body>
+</html>
---
+++ ticket.php
@@ -0,0 +1,28 @@
+<html>
+    <body>
+        <?php 
+            session_start();
+
+            // connect to database
+            $conn = mysqli_connect('localhost', 'root', '', 'Consert');
+
+            // initialize variable
+            $fname = $_POST["fname"];
+            $lname = $_POST["lname"];
+            $uemail = $_POST["email"];
+            $upnum= $_POST["pnum"];
+            $qty = $_POST["tix"];
+            $usr = $_SESSION['username'];
+            // make query
+            $sql = "INSERT INTO customers (FullName, LastName, Email, Pnum, Qty,username) VALUES ('$fname', '$lname', '$uemail', '$upnum', '$qty', '$usr')";
+
+            //connect query into database
+            $result = mysqli_query($conn, $sql);
+
+            if($result)
+                ECHO "YOU SUCCESFULL PURCHASE THE TICKET";
+            else
+                ECHO "You Failed to Purchase the ticket";
+        ?>
+    </body>
+</html>
