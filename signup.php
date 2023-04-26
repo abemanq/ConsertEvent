a3c22fa091b184a68c780ae1eae6e19c918598d6
mmusoddiq <musoddiqm@gmail.com>
2023-04-26 17:55:12 +0800

    huhuhuh

--- signup.php
+++ signup.php
@@ -1,22 +1,20 @@
-<html>
-<body>
 <?php
-$con=mysqli_connect("localhost", "root", "","concert") or die("Cannot connect to
-server.".mysqli_error($con));
+$con=mysqli_connect('localhost', 'root', '', 'concert') or die("Cannot connect to server." . mysqli_error($con));
 
- $username = $_POST["username"];
- $email = $_POST["email"];
- $inputPassword5 = $_POST["inputPassword5"];
- $inputPassword6 = $_POST["inputPassword5"];
+if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirm_password'])) {
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
 
+  $insert_sql="INSERT INTO customers VALUES(null, '$username', '$email', '$password', '$confirm_password')";
+  $sql_result = mysqli_query($con, $insert_sql) or die("Error in inserting data due to " . mysqli_error($con));
+  if($sql_result) {
+    echo "Successfully insert new data.";
+  } else {
+    echo "Error in inserting new data";
+  }
+}
 
- $insert_sql="INSERT INTO customers VALUES(null, '$username', '$email', '$inputPassword5', '$inputPassword6')";
- $sql_result =mysqli_query($con,$insert_sql) or die("Error in inserting data due to
-".mysql_error());
-if($sql_result)
- echo "Succesfully insert new data.";
- else
- echo "Error in inserting new data";
+mysqli_close($con);
 ?>
-</body>
-</html> 
